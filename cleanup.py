270ac317db6ba7a8a5ed528bc60390e1b9e1b17c
JochemHammer <jochemhammer06@gmail.com>
2026-08-20 17:19:51 +0200

    please... just please

--- cleanup.py
+++ cleanup.py
@@ -3,13 +3,14 @@ from os import remove
 
 path = "C:/Users/joche/Documents/GitHub/experiment/Battlers"
 folders = listdir(path)
+keywords = ["a","e"]
 
 for folder in folders:
     folderPath = path + '/' + folder
     files = listdir(folderPath)
 
     for file in files:
-        if "egg" in file:
+        if not any(word in file for word in keywords):
             filePath = folderPath + '/' + file
             print("removed " + filePath)
             remove(filePath)
--- imagecutter.py
+++ imagecutter.py
@@ -1,13 +1,10 @@
 from os import listdir
 import cv2
-import numpy as np
-from PIL import Image
 
 path = "C:/Users/joche/Documents/GitHub/experiment/Battlers"
 folders = listdir(path)
 
 M = 192
-
 
 for folder in folders:
     folderPath = path + '/' + folder
@@ -16,15 +13,10 @@ for folder in folders:
     for file in files:
         filePath = folderPath + '/' + file
         image = cv2.imread(filePath, cv2.IMREAD_UNCHANGED)
-
+        
         tiles = [image[x:x+M,y:y+M] for x in range(0,image.shape[0],M) for y in range(0,image.shape[1],M)]
-        print(tiles)
 
-        im = Image.fromarray(tiles[0])
-        im.save(folderPath + '/e' + file)
-        im = Image.fromarray(tiles[1])
-        im.save(folderPath + '/es' + file)
-        im = Image.fromarray(tiles[2])
-        im.save(folderPath + '/a' + file)
-        im = Image.fromarray(tiles[3])
-        im.save(folderPath + '/as' + file)
+        cv2.imwrite(folderPath + '/e' + file, tiles[0])
+        cv2.imwrite(folderPath + '/es' + file, tiles[1])
+        cv2.imwrite(folderPath + '/a' + file, tiles[2])
+        cv2.imwrite(folderPath + '/as' + file, tiles[3])
